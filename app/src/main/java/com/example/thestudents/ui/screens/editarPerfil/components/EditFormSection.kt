154fceaced0e51694877ca42c1cacf62907d3c25
DavidTobar20 <tobara.d@javeriana.edu.co>
2026-08-26 17:07:34 -0500

    Arreglo nombre carpeta EditarPerfil

--- app/src/main/java/com/example/thestudents/ui/screens/editarPerfil/components/EditFormSection.kt
+++ app/src/main/java/com/example/thestudents/ui/screens/editarPerfil/components/EditFormSection.kt
@@ -1,10 +1,9 @@
-package com.example.thestudents.ui.screens.EditarPerfil.components
+package com.example.thestudents.ui.screens.editarPerfil.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -26,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/example/thestudents/ui/screens/editarPerfil/components/HeaderEditarPerfil.kt
+++ app/src/main/java/com/example/thestudents/ui/screens/editarPerfil/components/HeaderEditarPerfil.kt
@@ -1,4 +1,4 @@
-package com.example.thestudents.ui.screens.EditarPerfil.components
+package com.example.thestudents.ui.screens.editarPerfil.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
