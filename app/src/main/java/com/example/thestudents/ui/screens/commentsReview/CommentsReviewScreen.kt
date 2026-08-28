22d28449d67af8357ecd0198f20af1706861d046
Javier Jaimes <javierajo06@hotmail.com>
2026-08-28 18:32:47 -0500

    arreglo error en importacion en CommentsReviewScreen.kt

--- app/src/main/java/com/example/thestudents/ui/screens/commentsReview/CommentsReviewScreen.kt
+++ app/src/main/java/com/example/thestudents/ui/screens/commentsReview/CommentsReviewScreen.kt
@@ -29,7 +29,6 @@ import com.example.thestudents.data.local.localStudentProvider
 import com.example.thestudents.ui.components.ReviewCard
 import com.example.thestudents.ui.screens.commentsReview.components.CommentInputFieldBar
 import com.example.thestudents.ui.screens.commentsReview.components.CommentItem
-import com.example.thestudents.ui.screens.commentsReview.components.ReviewCard
 import com.example.thestudents.ui.theme.TheStudentsTheme
 import com.example.thestudents.ui.utils.HeaderBack
 
